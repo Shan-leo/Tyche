f167e1a339325310f2cb8ad0b4444438d0e90699
Shan-leo <bizwithshan@gmail.com>
2023-07-03 20:04:21 +0530

    test commit

--- build/app.js
+++ build/app.js
@@ -1,2 +1,32 @@
 "use strict";
-console.log("Hello World");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const index_1 = __importDefault(require("./database/index"));
+index_1.default.query('SELECT * FROM product', (err, res) => {
+    if (err) {
+        console.log(err);
+        // res.status(500).json({message: 'error'})
+    }
+    else {
+        console.log(res.rows);
+    }
+});
+console.log('************8', index_1.default);
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true,
+}));
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Hello World!" });
+});
+app.post("/", (req, res) => {
+    res.status(200).json({ message: `success` });
+});
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
---
+++ build/database/index.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const { Pool } = require("pg");
+const dotenv = require('dotenv');
+dotenv.config();
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+exports.default = pool;
